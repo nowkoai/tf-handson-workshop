ac04864cc8c5352d011ed1e8f6e0784a7a7ddc06
nowkoai <hyamasit@gmail.com>
2022-03-14 22:47:57 +0900

    second commit

--- main.tf
+++ main.tf
@@ -1,2 +1,14 @@
 terraform {
 }
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+
+resource "aws_instance" "hello-tf-instance" {
+  ami = var.ami
+  count = var.hello_tf_instance_count
+  instance_type = var.hello_tf_instance_type
+}
